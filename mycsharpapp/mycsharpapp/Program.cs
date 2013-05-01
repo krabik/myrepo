4b0b2189be18303e441a8f5fd0916bed6fdef025
tomash <tomash.wrzonski@wonga.com>
2013-05-01 17:37:08 +0100

    add line

--- mycsharpapp/mycsharpapp/Program.cs
+++ mycsharpapp/mycsharpapp/Program.cs
@@ -14,6 +14,7 @@ namespace mycsharpapp
             for (int a = 10; a < 20; a = a + 1)
             {
                 Console.WriteLine("value of a: {0}", a);
+
             }
             Console.ReadLine();
         }
