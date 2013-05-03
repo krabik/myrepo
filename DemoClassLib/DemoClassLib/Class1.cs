5e87c9d8797595ff8f3ae57642144e4dc76bc64b
tomash <tomash.wrzonski@wonga.com>
2013-05-03 16:28:45 +0100

    add DemoClassLib and DemoNugetPckProgram projects

---
+++ DemoClassLib/DemoClassLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoClassLib
+{
+    public class MyMathClass
+    {
+        public int sum(int i, int j)
+        {
+            return i + j;
+        }
+
+        public double multi(int i, int j)
+        {
+            return i * j;
+        }
+
+    }
+}
