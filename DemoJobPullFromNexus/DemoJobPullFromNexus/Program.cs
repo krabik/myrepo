99645ffbfb6dc6cf901e97198ee7668824eb3473
tomash <tomash.wrzonski@wonga.com>
2013-05-29 15:58:17 +0100

    add packages files to repo

---
+++ DemoJobPullFromNexus/DemoJobPullFromNexus/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DemoClassLib;
+
+namespace DemoJobPullFromNexus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathClass m = new MyMathClass();
+
+            int res = m.sum(2, 7);
+            Console.WriteLine("Calling sum() function from nuget package named: DemoClassLib.2.0.0.nupkg\n\nResult: {0}", res);
+
+            Console.WriteLine("\nBye!");
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
