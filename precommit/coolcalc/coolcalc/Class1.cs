ad802aa94e082f63438bf32c599dd074ef57f7b3
tomash <tomash.wrzonski@wonga.com>
2013-05-31 10:25:10 +0100

    commit all files

---
+++ precommit/coolcalc/coolcalc/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorNs
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ precommit/mycsharpapp/mycsharpapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalculatorNs;
+
+namespace mycsharpapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator calc = new Calculator();
+            
+            Console.WriteLine("Calling dependency Calculator dll.");
+            Console.WriteLine("Passing 5+6 to Add() function.");
+            int res = calc.Add(5, 6);
+            Console.WriteLine("Result: {0:c}\nBye!", res);
+
+            Console.ReadLine();
+        }
+
+    }
+}
